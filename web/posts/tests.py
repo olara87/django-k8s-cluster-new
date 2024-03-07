9420d1b9dbd1be7711c7f56d711633f1308e528b
omar Lara <omarlara1987@gmail.com>
2024-03-07 11:52:02 -0500

    Updated tests.py

--- web/posts/tests.py
+++ web/posts/tests.py
@@ -1,7 +1,13 @@
 from django.test import TestCase
 
+from .models import Post
+
 # Create your tests here.
 class PostTestCase(TestCase):
 
+    def setUp(self):
+        Post.objects.create(title="Hello World")
+
     def test_failure(self):
-        self.assertTrue(False)
+        qs = Post.objects.all()
+        self.assertTrue(qs.exists())
